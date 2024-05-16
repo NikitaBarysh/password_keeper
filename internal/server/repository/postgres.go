7ac5769b9a86f59235d1d5106638f17c5f5a9d9c
nikita baryshnikov <super.kronk24@yandex.ru>
2024-05-16 19:46:58 +0300

    workflow правка v1.1.13

--- internal/server/repository/postgres.go
+++ internal/server/repository/postgres.go
@@ -4,7 +4,6 @@ package repository
 import (
 	"context"
 	"fmt"
-	"log"
 
 	_ "github.com/jackc/pgx/v5/stdlib"
 	"github.com/jmoiron/sqlx"
@@ -13,18 +12,15 @@ import (
 // InitDataBase - подключаемся к базе данных
 func InitDataBase(ctx context.Context, addr, port, dbName, user, pass string) (*sqlx.DB, error) {
 	url := fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=disable", addr, port, user, dbName, pass)
-	log.Println("url: ", url)
+
 	db, err := sqlx.Open("postgres", url)
 	if err != nil {
-		log.Println("err open: ", err)
 		return nil, fmt.Errorf("InitDataBase: err to create DB: %w", err)
 	}
 
 	if err = db.PingContext(ctx); err != nil {
-		log.Println("err ping: ", err)
 		return nil, fmt.Errorf("InitDataBase: err to ping DB: %w", err)
 	}
 
-	log.Println("db work: ", db)
 	return db, nil
 }
